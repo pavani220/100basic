b9df5c4a38e240e5e5596e67e8be3ab99e78cff1
Pavani <pavanibandi548@gmail.com>
2025-02-24 07:33:21 +0530

    file

---
+++ CountOfVowels.py
@@ -0,0 +1,8 @@
+a="pavani"
+count=0
+v=['a','e','i','o','u']
+for i in a:
+    if i in a:
+        if i in v:
+            count+=1
+print(count)
--- Palindrome.py
+++ Palindrome.py
@@ -1,4 +1,4 @@
-n="mom"
+'''n="mom"
 d=len(n)
 res=""
 i=d-1
@@ -8,5 +8,18 @@ while i>=0:
 print(res)
 if n==res:
     print("palindrome")
-    
-    
+
+'''
+a="121"
+u=int(a)
+temp=a
+rev=""
+while u>0:
+    d=u%10
+    rev=rev +str(d)
+    u=u//10
+print(rev)
+if rev==temp:
+    print("yes")
+else:
+    print("no")
